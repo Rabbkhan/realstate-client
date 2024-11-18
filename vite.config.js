34639ce6e721cccb9ec98a47e50f0cd73fd79751
Rabbilkhan <rabbilkhan125@gmail.com>
2024-11-18 17:10:19 +0530

    file changes of vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 
-  server: {
-    proxy: {
+  server:{
+    proxy:{
       '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:5000',
+        // target: 'http://localhost:5000',
+        target: 'https://realestate-api-0sig.onrender.com',
         secure: false,
       },
     },
   },
-  
 
 
   plugins: [react()],
