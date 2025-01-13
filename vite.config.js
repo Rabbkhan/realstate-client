3fd0b48b14a96cfe83f8eda6b8a82fc217f54896
Rabbilkhan <rabbilkhan125@gmail.com>
2025-01-14 03:52:13 +0530

    frontend url issue

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  // Dynamically determine the API target based on the mode
+  const isProduction = mode === "production";
+  const apiTarget = isProduction
+    ? "https://realestate-api-0sig.onrender.com"
+    : "http://localhost:5000";
 
-  server:{
-    proxy:{
-      '/api': {
-        // target: 'http://localhost:5000',
-        target: 'https://realestate-api-0sig.onrender.com',
-        secure: false,
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: apiTarget,
+          secure: false,
+          changeOrigin: true,
+        },
       },
     },
-  },
-
-
-  plugins: [react()],
-})
+    plugins: [react()],
+  };
+});
